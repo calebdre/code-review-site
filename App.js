323fc8dad638316bd1bae5ed6b5709ef6bbd87b7
Caleb Lewis <caleb_dre@yahoo.com>
2017-05-25 20:32:56 -0400

    added more fucntionality

--- App.js
+++ App.js
@@ -4,77 +4,14 @@ async function fetchReviews() {
     return t;
 }
 
-function attachEditor(index) {
-    const $line = $(".linenums div").eq(index);
-    $line.addClass("active");
-
-    const $elem = $("#code > div");
-    $elem.eq(index)
-        .data("haseditor", "true")
-        .append(
-            $("<textarea>", {
-                "class": "comment-editor"
-            }));
-    const $editor = $elem.find("textarea");
-    const augmentedEditor = new SimpleMDE({
-        element: $editor[0]
-    });
-    $editor.data("editor", augmentedEditor);
-}
-
-function detachEditor(index) {
-    $(".linenums div")
-        .eq(index)
-        .removeClass("active");
-
-    const $el = $("#code > div").eq(index);
-    const editor = $el.find("textarea")
-        .eq(0)
-        .data("editor");
-
-    if (editor === undefined ) {
-        return;
-    }
-
-    editor.toTextArea();
-    $el.find("textarea")
-        .eq(0)
-        .data("editor", null)
-
-    const find = $el
-        .find("div")
-        .get(0)
-        .outerHTML;
-
-    const newHtml = $(find).click(onLineClick);
-    $el.html(newHtml);
-}
-
-function calculateEditorPosition(currentActiveIndex) {
-    const $activeEls = $("#code > div.active");
-    const $lastActiveEl = $activeEls.last();
-    if ($activeEls.length > 0 && $lastActiveEl.index() !== currentActiveIndex) {
-        if ($activeEls.length !== 1) {
-            detachEditor(currentActiveIndex)
-        }
-        attachEditor($lastActiveEl.index());
-    }
-}
-
 function onLineClick(e) {
     const $target = $(e.currentTarget).parent();
-    const lastEditorPosition = $("#code > div.active").last().index();
 
     $target.toggleClass("active");
     if ($target.hasClass("active")) {
-        lineNumberCounter.increment();
-        // calculateEditorPosition(lastEditorPosition);
+        lineNumberCounter.increment($target.index() + 1);
     } else if (!$target.hasClass("active")) {
-        lineNumberCounter.decrement();
-        // if ($target.index() === lastEditorPosition) {
-        //     detachEditor(lastEditorPosition);
-        //     calculateEditorPosition($target.index());
-        // }
+        lineNumberCounter.decrement($target.index() + 1);
     }
 }
 
@@ -117,47 +54,160 @@ function attachListeners() {
     });
 
     previewButton.applyListener();
+    commentEditor.applyListeners();
 
     $("#code > div").mouseenter((e) => {
         if (window.isMouseDown === true) {
             const $el = $(e.currentTarget);
             $el.toggleClass("active");
             if ($el.hasClass("active")) {
-                lineNumberCounter.increment();
+                lineNumberCounter.increment($el.index() + 1);
             } else {
-                lineNumberCounter.decrement();
+                lineNumberCounter.decrement($el.index() + 1);
             }
         }
     });
 }
 
-function applyComment(comment) {
-    $(".linenums")
-        .find("div")
-        .eq(comment.line - 1)
-        .addClass("highlight")
-}
-
 function applyReview(item) {
     item["comments"].forEach((comment => {
-        applyComment(comment);
+        // applyComment(comment);
     }));
 }
 
-class LineNumberCounter {
+function composeHtml(data) {
+    const dataItem = Array.isArray(data) ? data[0] : data;
+
+    dataItem["tag"] = `<${dataItem["tag"]}>`;
+    const $el = $(dataItem["tag"]).addClass(dataItem["class"]);
+    if (dataItem.hasOwnProperty("data")) {
+        $el.html(dataItem["data"]);
+    }
+    if (dataItem.hasOwnProperty("children")) {
+        for (let i = 0; i < dataItem["children"].length; i++) {
+            $el.append(composeHtml(dataItem["children"][i]));
+        }
+    }
+
+    if (data.length > 1) {
+        const composedSibling = composeHtml(data.slice(1));
+        return $el.add(composedSibling);
+    }
+
+    return $el;
+}
+
+class CommentList {
+    constructor() {
+        this._$commentsContainer = $(".all-comments");
+        this._list = [];
+    }
+
+    /**
+     * @param UserComment[] list
+     */
+    setList(list) {
+        this._list = list;
+    }
+
+    render() {
+        var htmlComments = [];
+        for (let comment in this._list) {
+            const commentHtml = comment.generateHtml();
+            htmlComments.push(commentHtml);
+        }
+
+        this._$commentsContainer.append(htmlComments);
+    }
+
+    /**
+     * @param UserComment comment
+     */
+    append(comment) {
+        this._list.push(comment);
+        this._$commentsContainer.append(comment.generateHtml());
+    }
+}
+
+class UserComment {
+    constructor(commentData) {
+        this._data = commentData;
+        this._$commentsContainer = $(".all-comments");
+    }
+
+    generateHtml() {
+        var elData = [
+            {"class" : "user-comment", "tag": "div", "children": [
+                {"class":  "username", "tag": "p", "data": this._data["user"]},
+                {"class":  "time-since color-muted", "tag": "p", "data": this._data["postTime"]},
+                {"class":  "comment-wrapper", "tag": "div", "data": this._data["commentHtml"]}
+            ]
+            },
+            {"class": "comment-separator", "tag": "hr"}
+        ];
+
+        return composeHtml(elData);
+    }
+
+    applyHoverListener() {
+        // $("");
+    }
+}
+
+class CommentEditor {
+    constructor() {
+        this._textarea = $("#commentbox");
+        this._editor = new SimpleMDE({
+            element: this._textarea.get(0),
+            toolbar: false,
+            status: false
+        });
+        this._submitButton = $("#submit-comment-button");
+    }
+
+    applyListeners() {
+        this._submitButton.click((e) => {
+            const commentData = {
+                "user": "username",
+                "postTime" : "right now",
+                "commentHtml": showdownConverter.makeHtml(this.getCommentMarkdown()),
+                "lines": lineNumberCounter.getSelectedLines()
+            };
+
+            commentList.append(new UserComment(commentData));
+        });
+    }
+
+    togglePreview() {
+        this._editor.togglePreview();
+    }
+
+    getCommentMarkdown() {
+        return this._editor.value();
+    }
+}
+
+class SelectedLinesCounter {
     constructor() {
         this._count = 0;
         this._$counterEl = $(".lines-selected");
+        this.selectedLines = [];
     }
 
-    increment() {
+    increment(index) {
         this._count++;
+        this.selectedLines.push(index);
         this._refreshText();
     }
 
-    decrement() {
+    decrement(index) {
         this._count--;
+        this.selectedLines.splice(this.selectedLines.indexOf(index), 1);
         this._refreshText();
+    }
+
+    getSelectedLines() {
+        return this.selectedLines;
     }
 
     _refreshText() {
@@ -187,15 +237,11 @@ class PreviewButtonState {
     }
 }
 
-
-
-let commentEditor = new SimpleMDE({
-    element: $("#commentbox").get(0),
-    toolbar: false,
-    status: false
-});
+const commentList = new CommentList();
+const commentEditor = new CommentEditor();
 const previewButton = new PreviewButtonState();
-const lineNumberCounter = new LineNumberCounter();
+const lineNumberCounter = new SelectedLinesCounter();
+const showdownConverter = new showdown.Converter();
 
 $('document').ready(() => {
     generateLineNumbers();
@@ -207,5 +253,5 @@ $('document').ready(() => {
         r['reviews'].forEach((item) => {
             applyReview(item);
         });
-    })
-})
+    });
+});
